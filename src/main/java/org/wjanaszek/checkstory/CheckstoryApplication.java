8f5770d36f0e6c67b0c8292b06bb494f29fa7a5b
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-19 21:21:12 +0200

    Initial commit

---
+++ src/main/java/org/wjanaszek/checkstory/CheckstoryApplication.java
@@ -0,0 +1,12 @@
+package org.wjanaszek.checkstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckstoryApplication.class, args);
+	}
+}
