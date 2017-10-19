dcff4d6c06a653d03fd57f31ac700c775b53b915
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-19 21:34:30 +0200

    Work in progress

---
+++ src/main/java/org/wjanaszek/checkstory/configuration/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package org.wjanaszek.checkstory.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+}
---
+++ src/main/java/org/wjanaszek/checkstory/users/User.java
@@ -0,0 +1,57 @@
+package org.wjanaszek.checkstory.users;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String login;
+    private String email;
+    private String password;
+
+    User() {} // jpa only
+
+    public User(String login, String email, String password) {
+        this.login = login;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
