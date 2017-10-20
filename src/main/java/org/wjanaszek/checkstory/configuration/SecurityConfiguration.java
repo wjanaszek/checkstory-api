bf91c47a591380d0a64e61620c0e0329acd7bc9f
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-20 11:23:30 +0200

    Now working

--- src/main/java/org/wjanaszek/checkstory/configuration/SecurityConfiguration.java
+++ src/main/java/org/wjanaszek/checkstory/configuration/SecurityConfiguration.java
@@ -7,6 +7,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/api/*").permitAll();
     }
 }
