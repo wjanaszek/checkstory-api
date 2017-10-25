4ffc9b438d3b73f66ed4e0b612cf5931314151c4
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-25 23:30:44 +0200

    Little improvements

--- src/main/java/org/wjanaszek/checkstory/persistance/model/Photo.java
+++ src/main/java/org/wjanaszek/checkstory/persistance/model/Photo.java
@@ -10,13 +10,16 @@ import java.util.Date;
 public class Photo {
 
     @Id
-    @Column(name = "photo_no")
+    @Column(name = "id")
     private Long id;
 
     @ManyToOne
     @JoinColumn(name = "story_id")
     @JsonBackReference
     private Story story;
+
+    @Column(name = "path_to_file")
+    private String pathToFile;
 
     @Column(name = "create_date", nullable = false)
     private Date createDate;
@@ -25,4 +28,52 @@ public class Photo {
     private Date updateDate;
 
     protected Photo() {}
+
+    public Photo(Long id, Story story, String pathToFile, Date createDate, Date updateDate) {
+        this.id = id;
+        this.story = story;
+        this.pathToFile = pathToFile;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Story getStory() {
+        return story;
+    }
+
+    public void setStory(Story story) {
+        this.story = story;
+    }
+
+    public String getPathToFile() {
+        return pathToFile;
+    }
+
+    public void setPathToFile(String pathToFile) {
+        this.pathToFile = pathToFile;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
 }
