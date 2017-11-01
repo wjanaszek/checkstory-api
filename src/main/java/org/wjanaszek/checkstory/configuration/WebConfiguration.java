aab591afcb4f585651a5e4ffb02dd38ea4177dfc
wjanaszek <trash.wjanaszek@gmail.com>
2017-11-01 18:57:45 +0100

    Release 0.0.2

--- src/main/java/org/wjanaszek/checkstory/configuration/WebConfiguration.java
+++ src/main/java/org/wjanaszek/checkstory/configuration/WebConfiguration.java
@@ -1,5 +1,6 @@
 package org.wjanaszek.checkstory.configuration;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
